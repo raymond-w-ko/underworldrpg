7c5ce5d4a3a134508df93548c7e16566f4eb3da4
alex-laties <alex-laties@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-07 23:40:30 +0000

    added basic shapes for graphics engine

---
+++ UnderworldEngine/GraphicsEngine/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderworldEngine.GraphicsEngine
+{
+    class Point
+    {
+        float _x;
+        float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        float _y;
+        float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        float _z;
+        float Z
+        {
+            get
+            {
+                return _z;
+            }
+            set
+            {
+                _z = value;
+            }
+        }
+
+        public Point(float x, float y, float z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+}
