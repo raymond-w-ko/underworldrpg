e326133192783d6f27157296f018464ccbf569ff
raymond-w-ko <raymond-w-ko@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-08 18:33:19 +0000

    Added a Camera class and working on a BasicEffectManager.

---
+++ UnderworldEngine/GraphicsEngine/BasicEffectManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace UnderworldEngine.GraphicsEngine
+{
+    class BasicEffectManager
+    {
+        private Matrix worldMatrix;
+        public Matrix WorldMatrix
+        {
+            get
+            {
+                return worldMatrix;
+            }
+        }
+
+        private BasicEffect effect;
+        public BasicEffect Effect
+        {
+            get
+            {
+                return effect;
+            }
+        }
+
+        private Camera camera;
+
+        public BasicEffectManager(GraphicsDevice gd, Camera c)
+        {
+            this.camera = c;
+
+            this.effect = new BasicEffect(gd, null);
+
+            this.worldMatrix = Matrix.CreateTranslation(0, 0, 0);
+
+            effect.World = this.worldMatrix;
+            effect.View = camera.ViewMatrix;
+            effect.Projection = camera.ProjectionMatrix;
+
+            this.EnableVertexColor();
+        }
+
+        public void EnableVertexColor()
+        {
+            this.effect.VertexColorEnabled = true;
+        }
+
+        public void DisableVertexColor()
+        {
+            this.effect.VertexColorEnabled = false;
+        }
+    }
+}
---
+++ UnderworldEngine/GraphicsEngine/Camera.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace UnderworldEngine.GraphicsEngine
+{
+    class Camera
+    {
+        private Matrix viewMatrix;
+        public Matrix ViewMatrix
+        {
+            get
+            {
+                return viewMatrix;
+            }
+        }
+
+        private Matrix projectionMatrix;
+        public Matrix ProjectionMatrix
+        {
+            get
+            {
+                return projectionMatrix;
+            }
+        }
+
+        private Vector3 currentPosition;
+        private Vector3 currentTarget;
+        private Vector3 currentUpVector;
+
+        private float fov;
+        private float aspectRatio;
+        public class FovOutOfRangeException : System.ApplicationException { };
+
+        private float nearPlaneDistance;
+        private float farPlaneDistance;
+        public class PlaneDistanceException : System.ApplicationException { };
+
+        public Camera(Viewport viewport)
+        {
+            this.MoveTo(0, 0, 5);
+            this.LookAt(0, 0, 0);
+            this.SetFovDegrees(45);
+            
+            this.SetNearPlaneDistance(1.0f);
+            this.SetFarPlaneDistance(100.0f);
+
+            this.CalculateAspectRatio(viewport);
+        }
+
+        public void SetNearPlaneDistance(float dist)
+        {
+            if (dist < 1.0f) {
+                throw new PlaneDistanceException();
+            }
+            this.nearPlaneDistance = dist;
+
+            this.Update();
+        }
+
+        public void SetFarPlaneDistance(float dist)
+        {
+            if ((dist < 0) || (dist > 1000)) {
+                throw new PlaneDistanceException();
+            }
+            this.farPlaneDistance = dist;
+
+            this.Update();
+        }
+
+        public void CalculateAspectRatio(Viewport viewport)
+        {
+            this.aspectRatio = ((float)viewport.Width) / ((float)viewport.Height);
+
+            this.Update();
+        }
+
+        public void SetFovDegrees(float degrees)
+        {
+            if ((degrees <= 0) || (degrees >= 180)) {
+                throw new FovOutOfRangeException();
+            }
+
+            this.fov = MathHelper.ToRadians(degrees);
+
+            this.Update();
+        }
+
+        public void SetFovRadians(float radians)
+        {
+            if ((radians <= 0) || (radians >= Math.PI)) {
+                throw new FovOutOfRangeException();
+            }
+
+            this.fov = radians;
+
+            this.Update();
+        }
+
+        public void MoveTo(float x, float y, float z)
+        {
+            MoveTo(new Vector3(x, y, z));
+        }
+
+        public void MoveTo(Vector3 vector)
+        {
+            this.currentPosition = vector;
+            Update();
+        }
+
+        public void LookAt(float x, float y, float z)
+        {
+            this.LookAt(new Vector3(x, y, z));
+        }
+
+        public void LookAt(Vector3 vector)
+        {
+            currentTarget = vector;
+            Update();
+        }
+
+        public void SetUpVector(float x, float y, float z)
+        {
+            SetUpVector(x, y, z);
+        }
+
+        public void SetUpVector(Vector3 vector)
+        {
+            currentUpVector = vector;
+            Update();
+        }
+
+        private void Update()
+        {
+            viewMatrix = Matrix.CreateLookAt(currentPosition, currentTarget, currentUpVector);
+
+            if (this.nearPlaneDistance > this.farPlaneDistance) {
+                throw new PlaneDistanceException();
+            }
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(fov, aspectRatio, nearPlaneDistance,
+                farPlaneDistance);
+
+        }
+    }
+}
