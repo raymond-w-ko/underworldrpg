dc3da88e7cbf2ffa00eaa4c6656ed62157b6d11a
raymond-w-ko <raymond-w-ko@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-10 00:19:53 +0000

    Started work on a GameObject abstract class that will eventually represent renderable things ( heroes, monsters, objects ) inside the actual game.

---
+++ UnderworldEngine/Game/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace UnderworldEngine.Game
+{
+    abstract class GameObject
+    {
+        public Vector3 Position { get; set; }
+        public BoundingBox BoundingBox { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsVisible { get; set; }
+
+        public GameObject()
+        {
+            this.Position = Vector3.Zero;
+            this.BoundingBox = new BoundingBox();
+            this.IsActive = true;
+            this.IsVisible = true;
+        }
+
+        public abstract void Draw();
+    }
+}
