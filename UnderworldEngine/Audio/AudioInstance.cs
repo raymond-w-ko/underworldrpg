972cbdfa3972d3512f887d9fd71a4eb0f99d526e
Alexander Laties <alex.laties@gmail.com>
2010-09-11 02:53:08 +0000

    Added new classes for the audio engine to manage audio. Also added new music.

---
+++ UnderworldEngine/Audio/AudioInstance.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework;
+
+namespace UnderworldEngine.Audio
+{
+    class AudioInstance
+    {
+        /// <summary>
+        /// Wrapper for XACT .xap packages
+        /// Allows access to Sound and Wave Banks
+        /// </summary>
+        string name;
+        AudioEngine eng;
+        SoundBank sb;
+        WaveBank wb;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public AudioEngine AudioEngine
+        {
+            get
+            {
+                return eng;
+            }
+        }
+
+        public SoundBank SoundBank
+        {
+            get
+            {
+                return sb;
+            }
+        }
+
+        public WaveBank WaveBank
+        {
+            get
+            {
+                return wb;
+            }
+        }
+
+        public AudioInstance(string name)
+        {
+            this.name = name;
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            eng = new AudioEngine("Content\\Audio\\" + name + ".xgs");
+            sb = new SoundBank(eng, "Content\\Audio\\Sound Bank.xsb");
+            wb = new WaveBank(eng, "Content\\Audio\\Wave Bank.xwb");
+        }
+
+        public Cue GetCue(string cueName)
+        {
+            return sb.GetCue(cueName);
+        }
+    }
+}
---
+++ UnderworldEngine/Audio/AudioManager.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using System.Collections;
+using Microsoft.Xna.Framework;
+
+namespace UnderworldEngine.Audio
+{
+    class AudioManager
+    {
+        /// <summary>
+        /// Manages the loading, fetching, playing, and pausing of sounds and music
+        /// </summary>
+        
+        List<AudioInstance> instances;
+        LinkedList<Cue> currentlyPlaying;
+        Dictionary<Cue, AudioEmitter> cueToEmitter;
+        Dictionary<Cue, AudioListener> cueToListener;
+        Vector3 currentEmitterPosition;
+        Vector3 currentListenerPosition;
+        float volume;
+
+        public Vector3 CurrentEmitterPosition
+        {
+            get
+            {
+                return currentEmitterPosition;
+            }
+            set
+            {
+                currentEmitterPosition = value;
+            }
+        }
+
+        public Vector3 CurrentListeneverPosition
+        {
+            get
+            {
+                return currentListenerPosition;
+            }
+            set { currentListenerPosition = value; }
+        }
+
+        public float Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                volume = value % 2.0f;
+                UpdateVolume();
+            }
+        }
+
+        public AudioManager()
+        {
+            instances = new List<AudioInstance>();
+            currentlyPlaying = new LinkedList<Cue>();
+            cueToEmitter = new Dictionary<Cue, AudioEmitter>();
+            cueToListener = new Dictionary<Cue, AudioListener>();
+            currentListenerPosition = Vector3.Zero;
+            currentEmitterPosition = Vector3.Zero;
+        }
+
+        public bool AddSound(string name)
+        {
+            bool success;
+            AudioInstance inst;
+            
+            try
+            {
+                inst = new AudioInstance(name);
+                instances.Add(inst);
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+
+            return success;
+        }
+
+
+        public void PauseAll()
+        {
+            foreach (Cue cue in currentlyPlaying)
+            {
+                cue.Pause();
+            }
+        }
+
+        public void PlayAll()
+        {
+            foreach (Cue cue in currentlyPlaying)
+            {
+                cue.Play();
+            }
+        }
+        
+
+        public void ResumeAll()
+        {
+            foreach (Cue cue in currentlyPlaying)
+            {
+                cue.Resume();
+            }
+        }
+
+        public void StopAll()
+        {
+            foreach (Cue cue in currentlyPlaying)
+            {
+                cue.Stop(AudioStopOptions.Immediate);
+            }
+        }
+
+        private void UpdateVolume()
+        {
+            foreach(AudioInstance inst in instances)
+            {
+                inst.AudioEngine.GetCategory("Music").SetVolume(volume);
+            }
+        }
+
+        public Cue PlaySound(string name)
+        {
+            Cue sound = FetchSound(name);
+            sound.Play();
+            return sound;
+        }
+
+        private Cue FetchSound(string name)
+        {
+            Cue temp = null;
+
+            foreach (AudioInstance inst in instances)
+            {
+                try
+                {
+                    temp = inst.SoundBank.GetCue(name);
+                }
+                catch 
+                {
+                    temp = null;
+                }
+            }
+
+            if (temp == null)
+            {
+                throw new ArgumentNullException(name, "The requested sound was not found");
+            }
+
+            return temp;
+        }
+    }
+}
