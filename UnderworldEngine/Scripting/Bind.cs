9987a463cb9c3875f143e5fcd23b7fd95e499bd5
Alexander Laties <alex.laties@gmail.com>
2010-09-11 18:34:12 +0000

    Added stop, pause, and resume functionality to the interpreter. Began the workings of the bind command.

---
+++ UnderworldEngine/Scripting/Bind.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderworldEngine.Scripting
+{
+    class Bind : IInterpretable
+    {
+        #region IInterpretable Members
+
+        public void run(string function)
+        {
+            string[] command = function.Split(new Char[] { ' ' });
+            string keyToBind = command[1];
+            StringBuilder bindingTemp = new StringBuilder();
+            string binding;
+            for (int i = 2; i < command.Length; i++)
+            {
+                bindingTemp.Append(command[i]);
+            }
+
+            keyToBind = keyToBind.ToUpper();
+            switch (keyToBind)
+            {
+                case "A":
+                     
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
