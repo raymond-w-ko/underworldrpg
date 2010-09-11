ee3e4309195b882ea06175cb3b0a16d0a900e7d3
alex-laties <alex-laties@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-11 15:49:55 +0000

    Added the start of an interpreter for our scripting language. Included (but have not tested) the ability to load music.

---
+++ UnderworldEngine/Scripting/ILoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderworldEngine.Scripting
+{
+    interface ILoader
+    {
+        public bool load(string name);
+    }
+}
