55444154123a8ccfaff69d22675916cd86c14491
alex-laties <alex-laties@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-12 02:13:24 +0000

    Added the ability to change video resolution on load

--- UnderworldEngine/Content/Scripts/test.rs
+++ UnderworldEngine/Content/Scripts/test.rs
@@ -1,2 +1,7 @@
+setv width 1280
+setv height 720
+setv windowed
 bind a load soundpkg Music
 bind b music play Hello
+bind x music pause
+bind y music resume
---
+++ UnderworldEngine/Scripting/SetV.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderworldEngine.Scripting
+{
+    class SetV : IInterpretable
+    {
+        #region IInterpretable Members
+
+        public void run(string function)
+        {
+            string[] command = function.Split(new Char[] { ' ' });
+            command[1] = command[1].ToLower();
+            switch (command[1])
+            {
+                case "width":
+                    Game1.DefaultGraphics.PreferredBackBufferWidth =
+                        Convert.ToInt32(command[2]);
+                    break;
+                case "height":
+                    Game1.DefaultGraphics.PreferredBackBufferHeight =
+                        Convert.ToInt32(command[2]);
+                    break;
+                case "fullscreen":
+                    Game1.DefaultGraphics.IsFullScreen = true;
+                    break;
+                case "windowed":
+                    Game1.DefaultGraphics.IsFullScreen = false;
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
