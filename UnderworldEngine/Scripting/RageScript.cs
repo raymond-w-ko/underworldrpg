d885ce4f1f65942e4daf11d0a926647781bf31a9
alex-laties <alex-laties@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-15 02:22:49 +0000

    Added files that actually make binary scripts work

---
+++ UnderworldEngine/Scripting/RageScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UnderworldEngine.Scripting
+{
+    class RageScript
+    {
+        string source;
+        public MemoryStream stream;
+
+        public RageScript(string source)
+        {
+            this.source = source;
+            process();
+        }
+
+        private void process()
+        {
+            stream = new MemoryStream(Encoding.ASCII.GetBytes(source));
+        }
+
+        public void Unload()
+        {
+            this.source = null;
+            this.stream = null;
+        }
+    }
+}
