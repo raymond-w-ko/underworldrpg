014b13145726a56a729bf3d02f21deaff556389f
alex-laties <alex-laties@0a62fd88-2bfb-44e2-9deb-5e14209c912f>
2010-09-17 03:28:27 +0000

    Added new command repeat. repeat 5 x y will perform x y 5 times

---
+++ UnderworldEngine/Scripting/Repeat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderworldEngine.Scripting
+{
+    class Repeat : IInterpretable
+    {
+        #region IInterpretable Members
+
+        public void run(string function)
+        {
+            string[] command = function.Split(new Char[] { ' ' });
+            //TODO implement multiline
+            int repeats = Convert.ToInt32(command[1]);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i < command.Length; i++)
+            {
+                sb.Append(command[i]);
+                sb.Append(" ");
+            }
+
+            string func = sb.ToString();
+
+            for (int i = 0; i < repeats; i++)
+            {
+                Game1.console.Log("Running " + func);
+                Game1.interpreter.run(func);
+            }
+        }
+
+        #endregion
+    }
+}
